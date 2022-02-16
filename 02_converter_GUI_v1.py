e6e9dda8699917d59d5be6c777b46716f36e70e2
stevensc9277 <60583258+stevensc9277@users.noreply.github.com>
2022-02-17 10:45:17 +1300

    convert c to f is asked or f to c

--- 02_converter_GUI_v1.py
+++ 02_converter_GUI_v1.py
@@ -10,12 +10,12 @@ class Converter:
         background_colour = "light blue"
 
         # converter frame
-        self.converter_frame = Frame(width=300, bg=background_colour, pady=10)
+        self.converter_frame = Frame(bg=background_colour, pady=10)
         self.converter_frame.grid()
 
         # temperature converter heading (row 0)
-        self.temp_converter_label = Label(self.converter_frame, text="Temperature Converter", font=("Arial", "16", "bold"), bg=background_colour, padx=10, pady=10)
-        self.temp_converter_label.grid(row=0)
+        self.temp_heading_label = Label(self.converter_frame, text="Temperature Converter", font=("Arial", "16", "bold"), bg=background_colour, padx=10, pady=10)
+        self.temp_heading_label.grid(row=0)
 
         # user instructions (row 1)
         self.temp_instructions_label = Label(self.converter_frame, bg=background_colour, text="Type in the amount to be converted and then push on of the buttons below...", font=("Arial 10 italic"), justify=LEFT, wrap=250, padx=10, pady=10)
@@ -36,7 +36,7 @@ class Converter:
         self.to_f_button.grid(row=0, column=1)
 
         # answer label (row 4)
-        self.converted_label = Label(self.converter_frame, font="Arial 14 bold", fg="purple", bg=background_colour, pady=10)
+        self.converted_label = Label(self.converter_frame, font="Arial 14 bold", fg="purple", bg=background_colour, pady=10, text="Conversion goes here")
         self.converted_label.grid(row=4)
 
         # history / help button frame (row 5)
@@ -48,31 +48,60 @@ class Converter:
 
         self.help_button = Button(self.hist_help_frame, font="Arial 12 bold", text="Help", width=5)
         self.help_button.grid(row=0, column=1)  
+              
+    def temp_convert(self, low):
+        print(low) 
 
-    def temp_convert(self, to):
-        print(to) 
+        error = "#ffafaf" # pale pink background for when entry box has errors
 
         # retrieve amount entered into entry field
         to_convert = self.to_convert_entry.get()
 
         try:
             to_convert = float(to_convert)
-            print("yay")
+            has_errors = "no"
+
+            # check and convert to fahrenheit
+            if low == -273 and to_convert >= low:
+                fahrenheit = (to_convert * 9/5) + 32
+                to_convert = self.round_it(to_convert)
+                fahrenheit = self.round_it(fahrenheit)
+                answer = "{} degrees C is {} degrees F".format(to_convert, fahrenheit)
+
+            #  check and convert to centigrade
+            elif low == -459 and to_convert >= low:
+                celsius = (to_convert - 32) * 5/9
+                to_convert = self.round_it(to_convert)
+                celsius = self.round_it(celsius)
+                answer = "{} degrees F is {} degrees C".format(to_convert, celsius)
+
+            else:
+                # input is invalid (too cold!!)
+                has_errors = "yes"
+
+            # display answer
+            if has_errors == "no":
+                self.converted_label.configure(text=answer, fg="blue")
+                self.to_convert_entry.configure(bg="white")
+            
+            else:
+                self.converted_label.configure(text=answer, fg="red")
+                self.to_convert_entry.configure(bg=error)
+
+            # add answer to list for history 
 
         except ValueError:
-            print("oops")
+            self.converted_label.configure(text="Enter a number!", fg="red")
+            self.to_convert_entry.configure(bg=error)
 
-        # check amount is a valid number
-            
-        # convert to F
+    def round_it(self, to_round):
+        if to_round % 1 == 0:
+            rounded = int(to_round)
 
-        # convert to C
+        else:
+            rounded = round(to_round, 1)
 
-        # round!!
-
-        # display answer
-
-        # add answer to list for history                                                                          
+        return rounded                                                                             
 
 # main routine
 if __name__ == "__main__":
