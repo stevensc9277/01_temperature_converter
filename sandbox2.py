11dbdfeb2904e9b34d4b5d8464da7cae66b20f7a
stevensc9277 <60583258+stevensc9277@users.noreply.github.com>
2022-03-15 12:36:19 +1300

    added function that changes the color of multiple buttons

--- sandbox2.py
+++ sandbox2.py
@@ -1,33 +1,31 @@
 #Import required libraries
 from tkinter import *
-from turtle import color
 #Create an instance of tkinter frame
 win= Tk()
 #Define the geometry of the window
 win.geometry("750x250")
 
+def setup_hover(button_name):
+   color = button_name.cget("bg")
+   button_name.bind('<Enter>', lambda e: on_enter(e, button_name, color)) 
+
+   button_name.bind('<Leave>', lambda e: on_leave(e, color, button_name))
 #Define functions
-def on_enter(e, name):
-   name.config(background='green', foreground= "white")
+def on_enter(e, name, color):
+   name.config(background='dark '+color, foreground= "white")
 
 def on_leave(e, color, name):
    name.config(background=color, foreground= 'black')
    
 #Create a to_click
 
-to_click= Button(win, text= "Click Me", font= ('Helvetica 13 bold'), bg="yellow")
+to_click= Button(win, text= "Click Me", font= ('Helvetica 13 bold'), bg="green")
 to_click.pack(pady= 20)
 
 to_click2= Button(win, text= "Click Me", font= ('Helvetica 13 bold'), bg="blue")
 to_click2.pack(pady= 20)
 
-names = [to_click, to_click2]
-# find colors
-original = to_click.cget('bg')
-
-#Bind the Enter and Leave Events to the to_click
-names.bind('<Enter>', lambda e: on_enter(e, names[0])) 
-
-names.bind('<Leave>', lambda e: on_leave(e, original, names[1]))
+setup_hover(to_click)
+setup_hover(to_click2)
 
 win.mainloop()
